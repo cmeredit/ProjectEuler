6deb2600bd0e85125bd0bbc8e43b3750d35ed885
Connor Meredith <connor.meredith0913@gmail.com>
2022-08-16 15:38:09 -0400

    Write the final function (answer accepted by PE)

--- ProjectEuler-Scala/src/main/scala/io/github/cmeredit/problem19/Main19.scala
+++ ProjectEuler-Scala/src/main/scala/io/github/cmeredit/problem19/Main19.scala
@@ -1,5 +1,9 @@
 package io.github.cmeredit.problem19
 
-object Main19 {
+object Main19 extends App {
+  val monthStartInfo: IndexedSeq[(Int, Int, Int)] = PE19Util
+    .getMonthStartInfo(1900, PE19Util.monthNumbers("January"), PE19Util.dayNumbers("Monday"), 2000, PE19Util.monthNumbers("December"))
+    .filter({case (y, _, _) => y > 1900})
 
+  println(f"${monthStartInfo.count({case (_, _, d) => d == PE19Util.dayNumbers("Sunday")})} months during the twentieth century started with Sunday")
 }
