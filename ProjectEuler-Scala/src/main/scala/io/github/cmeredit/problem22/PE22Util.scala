8a2909e16cd4ef1096a20623ad08204ec32ae7aa
Connor Meredith <connor.meredith0913@gmail.com>
2022-08-16 16:30:25 -0400

    Write io and alph value functions

--- ProjectEuler-Scala/src/main/scala/io/github/cmeredit/problem22/PE22Util.scala
+++ ProjectEuler-Scala/src/main/scala/io/github/cmeredit/problem22/PE22Util.scala
@@ -1,5 +1,19 @@
 package io.github.cmeredit.problem22
 
+import scala.io.Source
+
 object PE22Util {
+
+  def getNamesFromFile(filename: String, delim: String = ","): Array[String] = {
+
+    val bufferedSource = Source.fromFile(filename)
+    val megaLine: String = bufferedSource.getLines().next()
+    bufferedSource.close()
+
+    megaLine.split(delim)
+
+  }
+
+  def getAlphabetValue(str: String): Int = str.toVector.map(_.toInt - 64).sum
 
 }
