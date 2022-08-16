4d5281a3b159bc6134376290fb410d95aca6fc05
Connor Meredith <connor.meredith0913@gmail.com>
2022-08-16 16:45:19 -0400

    Write code to compute the names-scores total (accepted by PE)

--- ProjectEuler-Scala/src/main/scala/io/github/cmeredit/problem22/PE22Main.scala
+++ ProjectEuler-Scala/src/main/scala/io/github/cmeredit/problem22/PE22Main.scala
@@ -2,4 +2,9 @@ package io.github.cmeredit.problem22
 
 object PE22Main extends App {
 
+  val sortedNames: Array[String] = PE22Util.getNamesFromFile("src/main/scala/io/github/cmeredit/problem22/p022_names.txt").sorted
+
+  val namesScores: Array[Long] = sortedNames.zipWithIndex.map({case (name, index) => PE22Util.getAlphabetValue(name).toLong * (index + 1).toLong})
+
+  println(f"Total of all names scores is: ${namesScores.sum}")
 }
