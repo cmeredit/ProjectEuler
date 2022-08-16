26afcce99aae32e55de98ec950a7641669be1b81
Connor Meredith <connor.meredith0913@gmail.com>
2022-08-16 13:11:20 -0400

    Write problem 1 brute force and clever solutions and write some tests for them.

--- ProjectEuler-Scala/src/test/scala/io/github/cmeredit/problem1/PE1Spec.scala
+++ ProjectEuler-Scala/src/test/scala/io/github/cmeredit/problem1/PE1Spec.scala
@@ -1,5 +1,27 @@
 package io.github.cmeredit.problem1
 
-class PE1Spec {
+import org.scalatest.funspec.AnyFunSpec
+
+class PE1Spec extends AnyFunSpec {
+
+  describe("The Problem 1 Utility Object") {
+    describe("when used in a brute-force style") {
+      it("should agree with the problem statement on an example input") {
+        assert(PE1Util.PE1BruteForce(10) === 23)
+      }
+    }
+
+    describe("when used in a clever style") {
+      it("should agree with the problem statement on an example input") {
+        assert(PE1Util.PE1(10) === 23)
+      }
+    }
+
+    describe("when used in either style") {
+      it("should produce the same output") {
+        (1 until 100).foreach(n => assert(PE1Util.PE1(n) === PE1Util.PE1BruteForce(n)))
+      }
+    }
+  }
 
 }
