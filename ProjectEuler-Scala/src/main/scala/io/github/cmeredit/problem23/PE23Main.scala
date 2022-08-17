2ce8452874434e521ceb372cd8d16c91a9866862
Connor Meredith <connor.meredith0913@gmail.com>
2022-08-17 10:52:33 -0600

    Write (accepted) solution to problem 23

--- ProjectEuler-Scala/src/main/scala/io/github/cmeredit/problem23/PE23Main.scala
+++ ProjectEuler-Scala/src/main/scala/io/github/cmeredit/problem23/PE23Main.scala
@@ -1,5 +1,14 @@
 package io.github.cmeredit.problem23
 
-object PE23Main {
+object PE23Main extends App {
 
+  // All values greater than this can be written as a sum of two abundant numbers
+  val highestCandidate: Int = 28123
+  // Note: This is the highest candidate by direct analysis, but the problem promises that this candidate fails.
+  val candidates: Vector[Int] = (1 until highestCandidate).toVector
+  val failures: Vector[Int] = PE23Util.getAbundantSumsBelow(highestCandidate)
+
+  val successes: Vector[Int] = candidates.diff(failures)
+
+  println(f"The sum of all positive integers that cannot be written as a sum of two abundant numbers is ${successes.sum}")
 }
