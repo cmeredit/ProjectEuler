6e6b6f9d00dcae221b817838d04ef21e73eba353
Connor Meredith <connor.meredith0913@gmail.com>
2022-08-17 10:57:59 -0600

    Write quick problem 23 test - agree with problem info

--- ProjectEuler-Scala/src/test/scala/io/github/cmeredit/problem23/PE23Spec.scala
+++ ProjectEuler-Scala/src/test/scala/io/github/cmeredit/problem23/PE23Spec.scala
@@ -1,5 +1,15 @@
 package io.github.cmeredit.problem23
 
-class PE23Spec {
+import org.scalatest.funspec.AnyFunSpec
+
+class PE23Spec extends AnyFunSpec {
+
+  describe("The Problem 22 Utility Object") {
+    it("should agree with the information given in the problem") {
+
+      assert(PE23Util.getAbundantSumsBelow(25) == Vector(24))
+
+    }
+  }
 
 }
