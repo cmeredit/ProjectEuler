4a0a1365d3784183621f3b06b216f6f55be3004f
Connor Meredith <connor.meredith0913@gmail.com>
2022-08-17 12:29:58 -0600

    Write (accepted) solution app for problem 26

--- ProjectEuler-Scala/src/main/scala/io/github/cmeredit/problem26/PE26Main.scala
+++ ProjectEuler-Scala/src/main/scala/io/github/cmeredit/problem26/PE26Main.scala
@@ -1,5 +1,9 @@
 package io.github.cmeredit.problem26
 
-object PE26Main {
+object PE26Main extends App {
+
+  val (d, cycleLength): (Int, Int) = (2 until 1_000).map(d => (d, PE26Util.getCycleLength(d))).maxBy(_._2)
+
+  println(f"$d has the longest reciprocal cycle length among the numbers less than 1,000, with a cycle length of $cycleLength")
 
 }
