1b16ca17c0826815d2ac72cd6d40943dcb3b8344
Connor Meredith <connor.meredith0913@gmail.com>
2022-08-19 16:45:46 -0400

    Write an (accepted) solution in a naiive functional style

--- ProjectEuler-Scala/src/main/scala/io/github/cmeredit/problem27/PE27Main.scala
+++ ProjectEuler-Scala/src/main/scala/io/github/cmeredit/problem27/PE27Main.scala
@@ -1,5 +1,7 @@
 package io.github.cmeredit.problem27
 
-object PE27Main {
+object PE27Main extends App {
+  val (a, b, l) = PE27Util.getWinningCoefficients()
 
+  println(f"Final winner: a=$a, p=$b with a length of $l primes!")
 }
--- ProjectEuler-Scala/src/main/scala/io/github/cmeredit/problem27/PE27Util.scala
+++ ProjectEuler-Scala/src/main/scala/io/github/cmeredit/problem27/PE27Util.scala
@@ -1,5 +1,64 @@
 package io.github.cmeredit.problem27
 
+import io.github.cmeredit.MathUtil
+
+import scala.collection.Searching.Found
+
 object PE27Util {
+
+
+
+  // Returns the pair (a, b) such that n^2 + an + b produces the longest sequence of primes for consecutive
+  // values of n, starting from n=0.
+  def getWinningCoefficients(aMin: Int = -999, aMax: Int = 999, bMax: Int = 999): (Int, Int, Int) = {
+
+    // Primes up to the highest polynomial value we might need to compute
+    val primes: Vector[Int] = MathUtil.Primes.primesLessThan(bMax * bMax + math.max(math.abs(aMin), aMax) * bMax + bMax)
+
+    val nat: LazyList[Int] = LazyList.from(0)
+
+    def getPrimeSeqLength(a: Int, b: Int): Int = {
+      nat.takeWhile(n =>
+        primes.search(n * n + a * n + b) match {
+          case Found(_) => true
+          case _ => false
+        }
+      ).length
+    }
+    
+    val candidatePairs: Vector[(Int, Int)] = for (
+      b <- MathUtil.Primes.primesLessThan(bMax + 1).reverse;
+      a <- aMin to aMax
+    ) yield (a, b)
+
+    val (firstA, firstB): (Int, Int) = candidatePairs.head
+
+    val (winningA, winningB, winningLength): (Int, Int, Int) = candidatePairs.tail.foldLeft((firstA, firstB, getPrimeSeqLength(firstA, firstB)))({case ((curWinnerA, curWinnerB, curWinnerLength), (nextA, nextB)) =>
+
+//      println(f"Testing a=$nextA, p=$nextB")
+      val maxSeqLength: Int = if (nextB > nextA) nextB - nextA else nextB
+
+      if (maxSeqLength < curWinnerLength) {
+        // This a/b pair is guaranteed not to produce enough primes
+//        println(f"This pair is guaranteed to lose")
+        (curWinnerA, curWinnerB, curWinnerLength)
+      } else {
+        // Need to check for victory...
+        val nextPrimeSeqLength: Int = getPrimeSeqLength(nextA, nextB)
+
+        if (nextPrimeSeqLength > curWinnerLength) {
+//          println(f"Found a new winning length of $nextPrimeSeqLength!")
+          (nextA, nextB, nextPrimeSeqLength)
+        } else {
+//          println(f"The old champion remains...")
+          (curWinnerA, curWinnerB, curWinnerLength)
+        }
+      }
+
+    })
+
+    (winningA, winningB, winningLength)
+
+  }
 
 }
