7de2c82ecbc84738fcc1607860395ce319ece634
Connor Meredith <connor.meredith0913@gmail.com>
2022-08-23 14:26:07 -0400

    Write (accepted) solution code

--- ProjectEuler-Scala/src/main/scala/io/github/cmeredit/problem31/PE31Main.scala
+++ ProjectEuler-Scala/src/main/scala/io/github/cmeredit/problem31/PE31Main.scala
@@ -1,5 +1,7 @@
 package io.github.cmeredit.problem31
 
-object PE31Main {
+object PE31Main extends App {
+
+  println(f"There are ${PE31Util.PS(Vector(1, 2, 5, 10, 20, 50, 100, 200), 200)} ways to make 2 pounds.")
 
 }
--- ProjectEuler-Scala/src/main/scala/io/github/cmeredit/problem31/PE31Util.scala
+++ ProjectEuler-Scala/src/main/scala/io/github/cmeredit/problem31/PE31Util.scala
@@ -2,4 +2,35 @@ package io.github.cmeredit.problem31
 
 object PE31Util {
 
+  private var psTable: Map[(Int, Int), Int] = Map()
+
+  def PS(S: Vector[Int], n: Int, k: Int): Int = {
+    if (psTable.contains((n, k))) {
+      psTable((n, k))
+    } else {
+      if (!S.contains(k)) {
+        0
+      } else {
+        val result: Int = if (k == 1) {
+          // Include the special case of k=1. If we don't include this, then we'll
+          // travel n steps down the recurrence
+          1
+        } else if (k > n) {
+          0
+        } else if (k == n) {
+          1
+        } else {
+          S.filter(j => j <= math.min(k, n-k)).map(j => PS(S, n-k, j)).sum
+        }
+
+        psTable = psTable.updated((n, k), result)
+        result
+      }
+    }
+  }
+
+  // Computes P_S(n), the number of S-partitions of n
+  def PS(S: Vector[Int], n: Int): Int = {
+    S.map(k => PS(S, n, k)).sum
+  }
 }
