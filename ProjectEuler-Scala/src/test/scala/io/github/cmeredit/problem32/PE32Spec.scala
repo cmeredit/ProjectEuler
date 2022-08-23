c489e31bbeb2b5b2f9bc156d6f58e9400211c251
Connor Meredith <connor.meredith0913@gmail.com>
2022-08-23 18:16:00 -0400

    Write quick test - problem 32 example

--- ProjectEuler-Scala/src/test/scala/io/github/cmeredit/problem32/PE32Spec.scala
+++ ProjectEuler-Scala/src/test/scala/io/github/cmeredit/problem32/PE32Spec.scala
@@ -1,5 +1,15 @@
 package io.github.cmeredit.problem32
 
-class PE32Spec {
+import org.scalatest.funspec.AnyFunSpec
+
+class PE32Spec extends AnyFunSpec {
+
+  describe("The Problem 32 Utility Object") {
+
+    it("should detect the example pandigital identity") {
+      assert(PE32Util.isIdentityPandigital(Vector(3, 9), Vector(1, 8, 6), Vector(7, 2, 5, 4)))
+    }
+
+  }
 
 }
