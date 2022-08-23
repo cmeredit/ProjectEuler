76730ea018ee971e6acd4bf40b701952c41cb69a
Connor Meredith <connor.meredith0913@gmail.com>
2022-08-23 19:40:28 -0400

    Write quick solution to problem 36

--- ProjectEuler-Scala/src/main/scala/io/github/cmeredit/problem36/PE36Main.scala
+++ ProjectEuler-Scala/src/main/scala/io/github/cmeredit/problem36/PE36Main.scala
@@ -1,5 +1,19 @@
 package io.github.cmeredit.problem36
 
-object PE36Main {
+import io.github.cmeredit.MathUtil
+
+object PE36Main extends App {
+
+  val doubleBasePalindromes: Seq[Int] = (1 to 1_000_000).filter(x => {
+    val digits = MathUtil.getDigits(x)
+    digits == digits.reverse
+  }).filter(x => {
+    val binaryDigits = MathUtil.getDigits(x, 2)
+    binaryDigits == binaryDigits.reverse
+  })
+
+  doubleBasePalindromes foreach println
+
+  println(f"The sum of all double-base palindromes below one million is ${doubleBasePalindromes.sum}")
 
 }
